e0b2086f0584e3e865fa306a50adbf2ec630f047
Clifford William <ccliffordwilliam@gmail.com>
2025-02-18 01:46:40 +0700

    feat: add user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,43 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  MinLength,
+} from 'class-validator';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  INSTRUCTOR = 'instructor',
+  STUDENT = 'student',
+}
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @MinLength(3)
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsEnum(UserRole)
+  @IsOptional() // Set to optional since some users might not need a specific role initially (like students)
+  role: UserRole;
+
+  @IsOptional()
+  @MinLength(3)
+  phone?: string; // Optional field for user phone number
+
+  @IsOptional()
+  @MinLength(5)
+  address?: string; // Optional field for user address
+
+  @IsOptional()
+  @MinLength(2)
+  instrument?: string; // Optional field for instructor's instrument (if role is instructor)
+}
