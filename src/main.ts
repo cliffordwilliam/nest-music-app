e79ce67e245b566d361e9752648974fd2390f155
Clifford William <ccliffordwilliam@gmail.com>
2025-02-20 02:20:20 +0700

    feat: add swag

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const options = new DocumentBuilder()
+    .setTitle('nest-music-app')
+    .setDescription(
+      'A NestJS-powered back-end with authentication, PostgreSQL, Prisma, and Redis. Built to showcase scalable API design and back-end architecture.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swag', app, document); // open browser thisAppDomain/swag e.g. localhost:3000/swag
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
